f4ad2513027dcba56165352b068c3042ce01a59a
Murilo Sartori <89427588+MuriloSart@users.noreply.github.com>
2023-09-19 22:11:37 -0300

    Build : Trigger para ataque avançado do Player
    Começando a criar um trigger para ataque do player ao pressionar o botão

--- core/src/game/collisions/ObstaclesBuilder.java
+++ core/src/game/collisions/ObstaclesBuilder.java
@@ -1,9 +1,10 @@
 package game.collisions;
 
-import static game.helper.Constants.PPM;
+import static game.helper.Constants.*;
 
 import com.badlogic.gdx.physics.box2d.Body;
 import com.badlogic.gdx.physics.box2d.BodyDef;
+import com.badlogic.gdx.physics.box2d.FixtureDef;
 import com.badlogic.gdx.physics.box2d.PolygonShape;
 import com.badlogic.gdx.physics.box2d.World;
 
@@ -22,18 +23,21 @@ public class ObstaclesBuilder
 	protected void setProperties(int x, int y, float width, float height)
 	{
 		groundBodyDef = new BodyDef();  
-		// Set its world position
-		groundBodyDef.position.set(x, y);
+		
+		groundBodyDef.position.set(x, y);// Setando a posição no Mundo Físico
 
-		// Create a body from the definition and add it to the world
-		groundBody = world.createBody(groundBodyDef);  
+		groundBody = world.createBody(groundBodyDef); //Criando o corpo no Mundo Físico
 
-		// Create a polygon shape
-		groundBox = new PolygonShape();  
-		// Set the polygon shape as a box which is twice the size of our view port and 20 high
-		// (setAsBox takes half-width and half-height as arguments)
+		groundBox = new PolygonShape(); // Criando uma Shape Poligonal
+		
 		groundBox.setAsBox(width/PPM, height/PPM);
-		// Create a fixture from our polygon shape and add it to our ground body  
-		groundBody.createFixture(groundBox, 0.0f).setUserData(this);
+		
+		FixtureDef fixtureDef = new FixtureDef();
+		fixtureDef.shape = groundBox;
+		fixtureDef.density = 1.0f;
+		fixtureDef.filter.categoryBits = BIT_WALL;
+		fixtureDef.filter.groupIndex = 0;
+		
+		groundBody.createFixture(fixtureDef).setUserData(this);
 	}
 }
---
+++ core/src/game/collisions/TriggerBuilder.java
@@ -0,0 +1,66 @@
+package game.collisions;
+
+import static game.helper.Constants.PPM;
+
+import com.badlogic.gdx.physics.box2d.Body;
+import com.badlogic.gdx.physics.box2d.BodyDef;
+import com.badlogic.gdx.physics.box2d.CircleShape;
+import com.badlogic.gdx.physics.box2d.FixtureDef;
+import com.badlogic.gdx.physics.box2d.PolygonShape;
+import com.badlogic.gdx.physics.box2d.World;
+
+public class TriggerBuilder
+{
+
+	public Body body;
+	protected BodyDef bodyDef;
+	public FixtureDef fixtureDef;
+
+	protected World world;
+	
+	public TriggerBuilder(World world) 
+	{
+		this.world = world;
+	}
+
+	public void setTrigger(float radius, float x, float y, String tag)
+	{
+		bodyDef = new BodyDef();
+		bodyDef.position.set(x, y); // Posicao inicial
+		bodyDef.type = BodyDef.BodyType.StaticBody;
+		body = world.createBody(bodyDef); // Criando o Objeto "body"
+		fixtureDef = new FixtureDef();
+		fixtureDef.density = 0f; // Densidade do corpo (afeta a massa)
+		fixtureDef.friction = 0f; // Coeficiente de friccao
+		fixtureDef.restitution = 0f; // Coeficiente de restituicao (elasticidade)
+		fixtureDef.isSensor = true;
+		
+		CircleShape shape = new CircleShape();
+		shape.setRadius(radius);
+		fixtureDef.shape = shape;
+
+		body.createFixture(fixtureDef).setUserData(this); // Adicione a Fixture ao corpo
+		shape.dispose();
+	}
+
+	public void setTrigger(float hx, float hy, float x, float y, String tag) 
+	{
+		bodyDef = new BodyDef();
+		bodyDef.position.set(x, y); // Posicao inicial
+		bodyDef.type = BodyDef.BodyType.StaticBody;
+		body = world.createBody(bodyDef); // Criando o Objeto "body"
+		fixtureDef = new FixtureDef();
+		fixtureDef.density = 0f; // Densidade do corpo (afeta a massa)
+		fixtureDef.friction = 0f; // Coeficiente de friccao
+		fixtureDef.restitution = 0f; // Coeficiente de restituicao (elasticidade)
+		fixtureDef.isSensor = true;
+
+		PolygonShape shape = new PolygonShape();
+		shape.setAsBox(hx / PPM, hy / PPM);
+		fixtureDef.shape = shape;
+		body.createFixture(fixtureDef).setUserData(this); // Criando Fixture e Setando seu Bit
+		
+		shape.dispose();
+	}
+
+}
